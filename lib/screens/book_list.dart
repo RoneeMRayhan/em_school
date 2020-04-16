55fb5daf16d23f21c1ee8c7e9f64bd749057ff32
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2020-04-16 13:02:32 +0600

    0=> TrainilgIconGesture

--- lib/screens/book_list.dart
+++ lib/screens/book_list.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:em_school/screens/home.dart';
+import 'package:em_school/screens/view_post.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class BookList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: StreamBuilder<QuerySnapshot>(
+      body: StreamBuilder<QuerySnapshot>(
         stream: Firestore.instance.collection('books').snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
@@ -19,6 +22,36 @@ class BookList extends StatelessWidget {
                   return new ListTile(
                     title: new Text(document['title']),
                     subtitle: new Text(document['author']),
+                    trailing: GestureDetector(
+                      child: GestureDetector(
+                        child: Icon(Icons.edit, color: Colors.amber),
+                        onTap: () {
+                          Fluttertoast.showToast(
+                              msg: "Data Edit",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.CENTER,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.red,
+                              textColor: Colors.white,
+                              fontSize: 16.0);
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage()));
+                        },
+                      ),
+                      //onTap:  document.documentID,//Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ViewPost()));
+                    ),
+                    /* onTap: () {
+                      Fluttertoast.showToast(
+                          msg: "Data inserted successfully",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                          fontSize: 16.0);
+                    }, */
                   );
                 }).toList(),
               );
