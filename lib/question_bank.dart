263cc049dc22fe2d743697d35d977def858b7225
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2020-05-01 00:31:31 +0600

    0=> Data added as a list...

--- lib/question_bank.dart
+++ lib/question_bank.dart
@@ -5,23 +5,33 @@ class QuestionBank extends StatelessWidget {
   QuestionBank({Key key}) : super(key: key);
   Firestore fdb = Firestore.instance;
 
+  List<Map> questions = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder<QuerySnapshot>(
-        stream: fdb
-            .collection("bcs")
-            //.orderBy("created_at", descending: true)
-            .snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) return const Text('Loading...');
-          final int messageCount = snapshot.data.documents.length;
-          return ListView.builder(
-            itemCount: messageCount,
-            itemBuilder: (_, int index) {
-              final DocumentSnapshot document = snapshot.data.documents[index];
-              final dynamic message = document['questionText'];
-              return ListTile(
+      body: buildBody(),
+    );
+  }
+
+  Widget buildBody() {
+    print(questions);
+    return StreamBuilder<QuerySnapshot>(
+      stream: fdb
+          .collection("bcs")
+          //.orderBy("created_at", descending: true)
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) return const Text('Loading...');
+        final int messageCount = snapshot.data.documents.length;
+        return ListView.builder(
+          itemCount: messageCount,
+          itemBuilder: (_, int index) {
+            final DocumentSnapshot document = snapshot.data.documents[index];
+            questions.add(document.data);
+            final dynamic message = document['option1'];
+            return GestureDetector(
+              child: ListTile(
                 trailing: IconButton(
                   onPressed: () => document.reference.delete(),
                   icon: Icon(Icons.delete),
@@ -32,11 +42,12 @@ class QuestionBank extends StatelessWidget {
                       : '<No message retrieved>',
                 ),
                 subtitle: Text('Message ${index + 1} of $messageCount'),
-              );
-            },
-          );
-        },
-      ),
+              ),
+              onTap: () => print(questions),
+            );
+          },
+        );
+      },
     );
   }
 }
